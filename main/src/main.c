49c2e8a1e9bb298b08b8e40040d19921bcd616f0
FranziskaKnoedtel <Franziska.Knoedtel@gmail.com>
2021-11-12 14:53:23 +0100

    changes to use new esp_io4edge core component

--- main/src/main.c
+++ main/src/main.c
@@ -13,27 +13,43 @@ limitations under the License.
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_log.h"
-#include "ota_server.h"
 #include "com_test.h"
 #include "can_test.h"
 #include "dcdc.h"
+#include "esp_err.h"
+#include "io4edge_core.h"
+#include "io4edge_machine.h"
 
-bool diagnostic_cb(void)
+static char *TAG = "main";
+
+// example hostname
+static const char* hostname = "ESP32S2";
+// example default instance name
+static const char* instance = "iou04-usb-ext";
+
+static bool application_is_working(void)
 {
-    /* signal that everything is okay */
+    ESP_LOGI(TAG, "application_is_working called");
     return true;
 }
 
 void app_main(void)
 {
-    /* initialize ota update */
-    init_ota();
+    static io4edge_core_config_t io4edge_core_config = {
+        .core_server_priority = 5, .application_is_working = application_is_working};
+
+    i2c_port_t i2c_port;
+    ESP_ERROR_CHECK(io4edge_iou_i2c_master_initialize(&i2c_port));
+    ESP_ERROR_CHECK(io4edge_new_iou_hw_inventory(i2c_port, &io4edge_core_config.hw_inventory_provider));
+    ESP_ERROR_CHECK(io4edge_core_init(hostname, instance));
 
     dcdc_converter_enable();
 
     com1_test_start();
     com2_test_start();
     can_test_start();
+
+    ESP_ERROR_CHECK(io4edge_core_start(&io4edge_core_config));
 
     for(;;){
         ESP_LOGI("main", "Alive");
