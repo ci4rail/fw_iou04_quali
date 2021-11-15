4fb577a0e615c8d9209aea2274f4b5894e10ba67
FranziskaKnoedtel <Franziska.Knoedtel@gmail.com>
2021-11-15 17:00:56 +0100

    Run all pre commit hooks.

--- main/src/com_test.c
+++ main/src/com_test.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <string.h>
+
+#include "driver/gpio.h"
+#include "driver/uart.h"
+#include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "driver/uart.h"
-#include "driver/gpio.h"
-#include "sdkconfig.h"
-#include "esp_log.h"
 #include "quali_uart_test.h"
+#include "sdkconfig.h"
 #include "test_status_report.h"
 
 #define RX_BUF_SIZE (1024)
@@ -18,16 +19,15 @@
 static const uart_config_t s_uart_config = {
     .baud_rate = 115200,
     .data_bits = UART_DATA_8_BITS,
-    .parity    = UART_PARITY_DISABLE,
+    .parity = UART_PARITY_DISABLE,
     .stop_bits = UART_STOP_BITS_1,
     .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
     .source_clk = UART_SCLK_APB,
 };
 
-
 void com1_test_start(void)
 {
-    uart_port_t uart_num = 0; // requires CONFIG_ESP_CONSOLE_NONE=y
+    uart_port_t uart_num = 0;  // requires CONFIG_ESP_CONSOLE_NONE=y
     quali_uart_test_handle_t *hdl;
     test_status_report_handle_t *sr_handle;
 
@@ -35,10 +35,10 @@ void com1_test_start(void)
 
     ESP_ERROR_CHECK(uart_driver_install(uart_num, RX_BUF_SIZE, TX_BUF_SIZE, 0, NULL, INTR_ALLOC_FLAGS));
     ESP_ERROR_CHECK(uart_param_config(uart_num, &s_uart_config));
-                                        /* TX, RX, RTS, CTS */
+    /* TX, RX, RTS, CTS */
     ESP_ERROR_CHECK(uart_set_pin(uart_num, 11, 13, 10, 12));
 
-    ESP_ERROR_CHECK(new_uart_test(&hdl, uart_num, sr_handle ));
+    ESP_ERROR_CHECK(new_uart_test(&hdl, uart_num, sr_handle));
 }
 
 void com2_test_start(void)
@@ -51,8 +51,8 @@ void com2_test_start(void)
 
     ESP_ERROR_CHECK(uart_driver_install(uart_num, RX_BUF_SIZE, TX_BUF_SIZE, 0, NULL, INTR_ALLOC_FLAGS));
     ESP_ERROR_CHECK(uart_param_config(uart_num, &s_uart_config));
-                                        /* TX, RX, RTS, CTS */
+    /* TX, RX, RTS, CTS */
     ESP_ERROR_CHECK(uart_set_pin(uart_num, 15, 17, 14, 16));
 
-    ESP_ERROR_CHECK(new_uart_test(&hdl, uart_num, sr_handle ));
+    ESP_ERROR_CHECK(new_uart_test(&hdl, uart_num, sr_handle));
 }
--- server_certs/ota_server_cert.h
+++ server_certs/ota_server_cert.h
@@ -16,4 +16,4 @@ limitations under the License.
 extern const uint8_t ota_server_cert_pem_start[] asm("_binary_ca_cert_pem_start");
 extern const uint8_t ota_server_cert_pem_end[] asm("_binary_ca_cert_pem_end");
 
-#endif //_OTA_SERVER_CERT_h
+#endif  //_OTA_SERVER_CERT_h
