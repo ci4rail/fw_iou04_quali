d61e34de7c40047b9343aa3caca450d562b3a3e9
FranziskaKnoedtel <Franziska.Knoedtel@gmail.com>
2022-03-16 15:21:50 +0000

    Added ttynvt component. Updated esp_io4edge. Adapt main to start ttynvt function.

--- main/src/main.c
+++ main/src/main.c
@@ -16,6 +16,9 @@ limitations under the License.
 #include "freertos/task.h"
 #include "io4edge.h"
 #include "io4edge_core.h"
+#include "driver/gpio.h"
+#include "driver/uart.h"
+#include "io4edge_ttynvt.h"
 
 static char* TAG = "main";
 
@@ -31,6 +34,9 @@ void app_main(void)
 {
     ESP_LOGI(TAG, "Start");
 
+    gpio_set_direction(GPIO_NUM_34, GPIO_MODE_OUTPUT);
+    gpio_set_level(GPIO_NUM_34, 1);
+
     io4edge_config_t io4edge_config = {
         .log_config =
             {
@@ -38,6 +44,23 @@ void app_main(void)
             },
     };
     ESP_ERROR_CHECK(io4edge_init(&io4edge_config));
+
+
+    io4edge_ttynvt_config_t ttynvt_config = {.instance = "iou04-com",
+        .instance_idx = 0,
+        .port = 10000,
+        .socket_listen_task_prio = 5,
+        .socket_rx_task_prio = 10,
+        .uart_num = UART_NUM_1,
+        .rx_buf_size = 1024,
+        .tx_buf_size = 1024,
+        .gpio_tx = 11,
+        .gpio_rx = 13,
+        .gpio_rts = 10,
+        .gpio_cts = 12,
+        .uart_task_prio = 11};
+
+    ESP_ERROR_CHECK(io4edge_ttynvt_new_instance(&ttynvt_config));
 
     static io4edge_core_config_t io4edge_core_config = {
         .core_server_priority = 5,
